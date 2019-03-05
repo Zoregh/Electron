acbed8d3b353b22b8bd43a5f796916332ac9ee4d
Zoregh <zoreghhakobyan@gmail.com>
2019-03-05 19:43:44 +0400

    fixed maximized window

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 const { app, BrowserWindow } = require('electron');
 
+let win;
+
 function createWindow () {
-  // Create the browser window.
-  let win = new BrowserWindow({});
-  win.maximize();
-  win.setMenu(null);
+  win = new BrowserWindow({
+    show: false,
+    // autoHideMenuBar: true, //auto-hide menu (calling btn = alt)
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
   win.loadFile('kendoUi.html');
+  win.maximize()
+  win.show()
+  win.setMenu(null); // disable menu
+
+  win.on('closed', () => {
+    win = null
+  });
 }
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
